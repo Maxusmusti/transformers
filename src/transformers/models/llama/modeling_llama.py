8d01dd866ced3f41e662c62f74e4fd09e4963960
Mustafa Eyceoz <meyceoz@redhat.com>
2024-12-18 14:14:04 -0500

    Update for llm hidden states
    Signed-off-by: Mustafa Eyceoz <meyceoz@redhat.com>

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -549,8 +549,8 @@ class LlamaDecoderLayer(nn.Module):
         B = hidden_mean.shape[0]
         hidden_max, _ = torch.max(hidden_mean.view(B, -1), dim=-1, keepdim=True) 
         hidden_min, _ = torch.min(hidden_mean.view(B, -1), dim=-1, keepdim=True)
-        hidden_factor = (hidden_mean - hidden_min.unsqueeze(2).unsqueeze(3)) / (hidden_max - hidden_min).unsqueeze(2).unsqueeze(3)
-        return hidden_factor.squeeze(0)
+        hidden_factor = (hidden_mean - hidden_min.unsqueeze(1)) / (hidden_max - hidden_min).unsqueeze(1)
+        return hidden_factor
 
     def forward(
         self,
